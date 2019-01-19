ddcdf4c07303e93733dff67f4a3e50bdc4326d85
NicolasRementeria <nicolas.rementeria@gmail.com>
2019-01-19 18:42:50 -0300

    Added function to test a two-parameter function

---
+++ Get-FreeDiskSpace.ps1
@@ -0,0 +1,15 @@
+﻿#Get the free disk space of the targeted computer
+
+Function Get-FreeDiskSpace($drive,$computer)
+{
+    $driveData = Get-WmiObject -class win32_LogicalDisk `
+    -computername $computer -filter “Name = '$drive'"
+    "
+    $computer free disk space on drive $drive
+    $("{0:n2}" -f ($driveData.FreeSpace/1MB)) MegaBytes
+    "
+}
+
+#Get-FreeDiskSpace -drive "C:" -computer "C10"
+
+Get-FreeDiskSpace -drive "C:" -computer localhost
