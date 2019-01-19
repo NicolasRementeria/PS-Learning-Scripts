6f4850350a14c638d30d1ec344e3e0fab22bfd0f
NicolasRementeria <nicolas.rementeria@gmail.com>
2019-01-19 18:50:08 -0300

    Updated the function with Help

--- Get-FreeDiskSpace.ps1
+++ Get-FreeDiskSpace.ps1
@@ -1,7 +1,24 @@
 ﻿#Get the free disk space of the targeted computer
 
-Function Get-FreeDiskSpace($drive,$computer)
+Function Get-FreeDiskSpace($drive,$computer, [switch]$help)
 {
+    If($help)
+    {
+        $helpstring = @"
+        NAME
+        Get-FreeDiskSpace
+        SYNOPSIS
+        Get the free disk space of the targeted computer
+        SYNTAX
+        Get-FreeDiskSpace [[-drive] [string]] [[-computer] [string]] [-help]
+        EXAMPLE
+        Get-FreeDiskSpace -drive "C:" -computer localhost
+        This command gives the C Disk Space. The C Disk space 
+        are from the localhost computer.
+"@
+$helpString
+        break #exits the function early
+    }
     $driveData = Get-WmiObject -class win32_LogicalDisk `
     -computername $computer -filter “Name = '$drive'"
     "
@@ -13,3 +30,5 @@ Function Get-FreeDiskSpace($drive,$computer)
 #Get-FreeDiskSpace -drive "C:" -computer "C10"
 
 Get-FreeDiskSpace -drive "C:" -computer localhost
+
+Get-FreeDiskSpace -help
