055bdcab554a23b8e401f61b941aa7101cdbb9d9
NicolasRementeria <nicolas.rementeria@gmail.com>
2019-01-19 20:45:10 -0300

    Last excercises of learning PS Manual

---
+++ Get-FilesByDate.ps1
@@ -0,0 +1,15 @@
+﻿#Functional excercise
+
+function Get-FilesByDate{
+    Param(
+        [string[]]$fileTypes, 
+        [int]$month, 
+        [int]$year, 
+        [string[]]$path)
+    Get-ChildItem -Path $path -Include $fileTypes -Recurse | 
+    Where-Object { $_.lastwritetime.month -eq $month -AND $_.lastwritetime.year -eq $year }
+} #end function 
+
+#Example test Get-FilesByDate
+
+Get-FilesByDate -fileTypes *.txt -month 1 -year 2019 -path c:\
---
+++ Get-FilesByDateV2.ps1
@@ -0,0 +1,20 @@
+﻿#Functional excercise
+
+function Get-FilesByDate{
+    Param(
+        [string[]]$fileTypes = @("*.txt", "*.exe"), 
+        [Parameter(Mandatory=$true)]
+        [int]$month,
+        [Parameter(Mandatory=$true)]
+        [int]$year,
+        [Parameter(Mandatory=$true)]
+        [string[]]$path)
+    Get-ChildItem -Path $path -Include $fileTypes -Recurse | 
+    Where-Object { $_.lastwritetime.month -eq $month -AND $_.lastwritetime.year -eq $year }
+} #end function 
+
+#Example test Get-FilesByDate
+
+#Get-FilesByDate -month 1 -year 2019 -path "C:\"
+
+Get-FilesByDate
